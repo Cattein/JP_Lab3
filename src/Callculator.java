8629eca034d2d0642885428f44661bd9c6f7006e
MashaGuzhva <mguzhvao.o@gmail.com>
2025-11-25 22:43:13 +0100

    Okno

---
+++ src/Callculator.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Callculator {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Calculator");
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,400);
+        frame.setLocationRelativeTo(null);
+
+    }
+}
