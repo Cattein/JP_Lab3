1892a78b0c0cc325be13d5bbc74606755b805755
MashaGuzhva <mguzhvao.o@gmail.com>
2025-11-26 02:08:44 +0100

    start

---
+++ src/Hello.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Okno główne");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 200);
+        frame.setLocationRelativeTo(null);
+
+
+
+
+
+
+
+        frame.setVisible(true);
+
+    }
+
+
+
+
+}
