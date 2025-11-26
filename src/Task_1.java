23c18206629514a6932c4ad994cb6764911a36fb
MashaGuzhva <mguzhvao.o@gmail.com>
2025-11-26 01:49:24 +0100

    JFrame

---
+++ src/Task_1.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class Task_1  {
+
+    public Task_1() {
+
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Prosta aplikacja");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 300);
+        frame.setLocationRelativeTo(null);
+
+        frame.setVisible(true);
+
+    }
+}
