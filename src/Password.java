e9ee403e038930bd7bbfaeaa479880034d67c663
MashaGuzhva <mguzhvao.o@gmail.com>
2025-11-26 23:41:04 +0100

    add checking password and marks

--- src/Password.java
+++ src/Password.java
@@ -14,8 +14,34 @@ public class Password extends JFrame{
         panel.add(passwordLabel);
         JPasswordField passwordField = new JPasswordField(10);
         panel.add(passwordField);
+        JButton checkButton = new JButton("Check");
+        panel.add(checkButton);
 
 
+
+        checkButton.addActionListener(e -> {
+            String password = new String(passwordField.getPassword());
+            boolean len   = password.length() >= 8;
+            boolean letterBig = password.matches(".*[A-Z].*");
+            boolean Special = password.matches(".*[^a-zA-Z0-9].*");
+            if (len && letterBig && Special) {
+                JOptionPane.showMessageDialog(this, "Good");
+
+            } else {
+                StringBuilder msg = new StringBuilder("Password is trash:\n");
+
+                if (!len) {
+                    msg.append("- min. 8 signs\n");
+                }
+                if (!letterBig) {
+                    msg.append("- min. 1 Upper liter (A-Z)\n");
+                }
+                if (!Special) {
+                    msg.append("- min. 1 sign  special (np:@#$%)\n");
+                }
+                JOptionPane.showMessageDialog(this, msg);
+
+            }});
     }
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
