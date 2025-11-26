f9066278e9c2211fc90e8ef832495897f31afab6
MashaGuzhva <mguzhvao.o@gmail.com>
2025-11-26 02:25:10 +0100

    send hello user

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Hello {
     private static JLabel Hejka;
@@ -12,17 +14,23 @@ public class Hello {
         frame.setLocationRelativeTo(null);
 
 
-        Hejka = new JLabel("Hello,", SwingConstants.CENTER);
-        JButton askNameButton = new JButton("Podaj imię");
+        Hejka = new JLabel("Hi!", SwingConstants.CENTER);
+        JButton askNameButton = new JButton("Type your name");
 
-
-
-
+        frame.add(panel);
         panel.add(Hejka, BorderLayout.CENTER);
         panel.add(askNameButton, BorderLayout.SOUTH);
 
-        frame.add(panel);
-
+        askNameButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String name = JOptionPane.showInputDialog(frame, "write your name:");
+                if (name != null && !name.trim().isEmpty()) {
+                    String message = "Hello, " + name + "!";
+                    Hejka.setText(message);
+                }
+            }
+        });
 
 
         frame.setVisible(true);
