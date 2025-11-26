977eeb7782b43d46a9cc87e38c4a435a0053a8f2
MashaGuzhva <mguzhvao.o@gmail.com>
2025-11-26 23:23:13 +0100

    Add password field

--- src/Password.java
+++ src/Password.java
@@ -2,11 +2,19 @@ import javax.swing.*;
 
 public class Password extends JFrame{
     public Password() {
-
-        setTitle("Really strong password ");
+        setTitle("Real strong Password ");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(600, 400);
         setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        add(panel);
+
+        JLabel passwordLabel = new JLabel("Password:");
+        panel.add(passwordLabel);
+        JPasswordField passwordField = new JPasswordField(10);
+        panel.add(passwordField);
+
 
     }
     public static void main(String[] args) {
