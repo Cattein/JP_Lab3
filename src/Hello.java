2517669c7d1b9b4218b250e829bb33b23f1c0b4a
MashaGuzhva <mguzhvao.o@gmail.com>
2025-11-26 02:14:28 +0100

    text+ botoon

--- src/Hello.java
+++ src/Hello.java
@@ -1,16 +1,27 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Hello {
+    private static JLabel Hejka;
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Okno główne");
+        JFrame frame = new JFrame("Hiiiiiiii");
+        JPanel panel = new JPanel(new BorderLayout(10, 10));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 200);
         frame.setLocationRelativeTo(null);
 
 
+        Hejka = new JLabel("Hello,", SwingConstants.CENTER);
+        JButton askNameButton = new JButton("Podaj imię");
 
 
+
+
+        panel.add(Hejka, BorderLayout.CENTER);
+        panel.add(askNameButton, BorderLayout.SOUTH);
+
+        frame.add(panel);
 
 
 
