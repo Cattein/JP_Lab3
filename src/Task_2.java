12226b9e38107bd373ef6bd573e95d5b1b5fcf9b
MashaGuzhva <mguzhvao.o@gmail.com>
2025-11-26 22:24:25 +0100

    Add object to change

--- src/Task_2.java
+++ src/Task_2.java
@@ -1,13 +1,28 @@
 import javax.swing.*;
-import java.awt.*;
+
 
 public class Task_2 extends JFrame {
+    private JLabel obj;
 
     public Task_2() {
         setTitle("Dynamiczny interfejs ");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(600, 400);
         setLocationRelativeTo(null);
+
+        JPanel Panel = new JPanel(new java.awt.BorderLayout());
+        setContentPane(Panel);
+
+        JPanel nextWindow = new JPanel(null);
+        Panel.add(nextWindow, java.awt.BorderLayout.CENTER);
+
+        obj = new JLabel(":)");
+        obj.setHorizontalAlignment(SwingConstants.CENTER);
+        obj.setBounds(200, 100, 200, 50);
+
+
+
+        nextWindow.add(obj);
 
     }
 
